6fb827461709ab6991cd30f033ba39228df39f58
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-12-03 10:14:54 +0200

    task 4

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,31 @@
+import { createClient, redis } from 'redis';
+
+const localhost = '127.0.0.1';
+const client = createClient({
+  host: localhost,
+  port: 6379,
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.hset('HolbertonSchools', 'Portland', '50', redis.print);
+client.hset('HolbertonSchools', 'Seattle', '80', redis.print);
+client.hset('HolbertonSchools', 'New York', '20', redis.print);
+client.hset('HolbertonSchools', 'Bogota', '20', redis.print);
+client.hset('HolbertonSchools', 'Cali', '40', redis.print);
+client.hset('HolbertonSchools', 'Paris', '2', redis.print);
+
+// Get all the fields and values in the hash
+client.hgetall('HolbertonSchools', (err, obj) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(obj);
+  }
+});
