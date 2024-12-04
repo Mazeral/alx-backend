707060ef52b18fa2418f2e71d280570d04bf0fad
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-12-04 16:09:11 +0200

    task 10 testing complete

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,21 +1,32 @@
 import kue from 'kue';
 
-const push_notification_code_3 = kue.createQueue();
 function createPushNotificationsJobs(jobs, queue) {
   if (!Array.isArray(jobs)) {
     throw Error('Jobs is not an array');
   }
   jobs.forEach((element) => {
-    const job = push_notification_code_3.create('job', element).save((err) => {
+    const job = queue.create('push_notification_code_3', element);
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+
+    job.save((err) => {
       if (!err) {
         console.log(`Notification job created: ${job.id}`);
-      } else if (err) {
-        console.log(`Notification job ${job.id} failed ${err}`);
+      } else {
+        console.log(`Notification job ${job.id} failed: ${err}`);
       }
     });
-		  job.on('complete', () => {
-      console.log(`Notification job ${job.id} completed`);
-		  console.log(`Notification job ${job.id} ${(jobs.indexOf(element) + 1) / jobs.length}% complete`);
-		  });
   });
 }
+
+export default createPushNotificationsJobs;
